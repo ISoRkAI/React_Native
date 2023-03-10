1f8b00986ec397adbdb5aa9969b716db09b7c6a8
Dmytro Soroka <dimasoroka17@gmail.com>
2023-03-10 16:55:32 +0200

    Login components

--- myNewProject/components/RegistrationScreen.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import { View, Image, Text } from 'react-native';
-// import styles from '../style/style';
-
-// const Registration = () => (
-// );
-
-// export default Registration;
